b9d38a45875cbb520aaf815642cde0240b032642
Aaron Pius <piuschbz@gmail.com>
2024-02-02 20:05:29 +0000

    Implementes cities blueprint, get and post methods for city

---
+++ api/v1/views/cities.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+""" City Class Flask blueprint"""
+
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+from models.city import City
+
+@app_views.route('states/<state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
+def get_state_cities(state_id):
+    """ Routes all cities of a state."""
+
+    states = storage.all(State)
+    if not 'State.' + state_id in states:
+        abort(404)
+    state = states['State.' + state_id]
+
+    cities = [city.to_dict() for city in state.cities]
+
+    return jsonify(cities)
+
+
+@app_views.route('cities/<city_id>', methods=['GET'], strict_slashes=False)
+def get_city(city_id):
+    """ Routes the city resource """
+
+    cities = storage.all(City)
+
+    if not 'City.' + city_id in cities:
+        abort(404)
+
+    return jsonify(cities['City.' + city_id].to_dict())
+
+@app_views.route('states/<state_id>/cities', strict_slashes=False,
+                 methods=['POST'])
+def post_state_cities(state_id):
+    """ Post cities data to storage with linked state_id."""
+
+    states = storage.all(State)
+
+    if not 'State.' + state_id in states:
+        abort(404)
+    if not request.get_json():
+        return 'Not a JSON', 400
+    if not request.get_json()['name']:
+        return 'Missing name', 400
+    city = City(name=request.get_json()['name'], state_id=state_id)
+    for attribute in request.get_json():
+        if attribute not in ('name', 'state_id'):
+            city[attribute] = request.get_json()[attribute]
+    city.save()
+    return jsonify(city.to_dict()), 200
